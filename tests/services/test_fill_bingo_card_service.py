775896b85b362e08944ce143dd1e204412085a19
ch-iv <icheppy101@gmail.com>
2025-10-11 14:35:39 -0400

    Add tests for `detect_image_cells_service.py`

--- tests/services/test_fill_bingo_card_service.py
+++ tests/services/test_fill_bingo_card_service.py
@@ -1,7 +1,7 @@
 import sys
 from pathlib import Path
 
-sys.path.insert(0, str(Path(__file__).parent.parent))
+sys.path.insert(0, str(Path(__file__).parent.parent.parent))
 
 from pytest import fixture
 
