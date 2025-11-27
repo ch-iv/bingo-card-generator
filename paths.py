804770b727ddb3d846d52d85b4e742eedb0c0b9b
ch-iv <icheppy101@gmail.com>
2025-11-26 19:21:01 -0500

    Update font paths

--- paths.py
+++ paths.py
@@ -3,3 +3,4 @@ from pathlib import Path
 file_save_path = Path(__file__).parent / "runtime" / "uploads"
 file_identity_db_path = Path(__file__).parent / "runtime" / "file_identity.db"
 generated_cards_path = Path(__file__).parent / "runtime" / "generated_cards"
+fonts_path = Path(__file__).parent / "web" / "static" / "fonts"
